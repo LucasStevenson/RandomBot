f10814cf78d5b380afc204dd9c674161f3dc1331
LucasStevenson <45473315+LucasStevenson@users.noreply.github.com>
2019-02-13 22:28:32 -0800

    fixed older file bugs

--- Info/userinfo.js
+++ Info/userinfo.js
@@ -9,104 +9,103 @@ module.exports.run = async (bot, message, args) => {
         luminosity: 'random',
         hue: 'random'
     });
-     mention = message.mentions.members.first() || message.guild.members.get(args[0]); //if mentioned user
+    var mention = message.mentions.members.first() || message.guild.members.get(args[0]); //if mentioned user
 
-     //status emotes
-     const online = bot.emojis.find(emoji => emoji.name === "online");
-     const dnd = bot.emojis.find(emoji => emoji.name === "dnd");
-     const idle = bot.emojis.find(emoji => emoji.name === "idle");
-     const offline = bot.emojis.find(emoji => emoji.name === "offline");
-     const streaming = bot.emojis.find(emoji => emoji.name === "streaming");
+    //status emotes
+    const online = bot.emojis.find(emoji => emoji.name === "online");
+    const dnd = bot.emojis.find(emoji => emoji.name === "dnd");
+    const idle = bot.emojis.find(emoji => emoji.name === "idle");
+    const offline = bot.emojis.find(emoji => emoji.name === "offline");
+    const streaming = bot.emojis.find(emoji => emoji.name === "streaming");
      
-     //dates when author created and joined server
-     var userCreated = moment(message.author.createdAt).format('llll');
-     var userJoined = moment(message.member.joinedAt).format('llll');
+    //dates when author created and joined server
+    var userCreated = moment(message.author.createdAt).format('llll');
+    var userJoined = moment(message.member.joinedAt).format('llll');
      
-     //author status
-     let authorStatus = message.member.presence.status;
+    //author status
+    let authorStatus = message.member.presence.status;
      
-     if(!mention){ //if no one was mentioned
+    if(!mention){ //if no one was mentioned
     
-        let uEmbed = new discord.RichEmbed()
+      let uEmbed = new discord.RichEmbed()
 
-        uEmbed.setTitle(`Information for ${message.author.username}`)
-        switch(authorStatus){
-            case "online":
-              uEmbed.setDescription(`${online}**Online**`);
-              break;
-            case "dnd":
-              uEmbed.setDescription(`${dnd}**Do Not Disturb**`);
-              break;
-            case "idle":
-              uEmbed.setDescription(`${idle}**Idle**`);
-              break;
-            case "streaming":
-              uEmbed.setDescription(`${streaming}**Streaming**`);
-              break;
-            default:
-              uEmbed.setDescription(`${offline}**Invisible or offline**`);
-              break;
-          }
+      uEmbed.setTitle(`Information for ${message.author.username}`)
+      switch(authorStatus){
+          case "online":
+            uEmbed.setDescription(`${online}**Online**`);
+            break;
+          case "dnd":
+            uEmbed.setDescription(`${dnd}**Do Not Disturb**`);
+            break;
+          case "idle":
+            uEmbed.setDescription(`${idle}**Idle**`);
+            break;
+          case "streaming":
+            uEmbed.setDescription(`${streaming}**Streaming**`);
+            break;
+          default:
+            uEmbed.setDescription(`${offline}**Invisible or offline**`);
+            break;
+        }
         
-          uEmbed.addField("Username", `${message.author.tag}`, true)
-          uEmbed.setColor(color)
-          uEmbed.addField("UserID", `${message.author.id}`, true)
-          if(!message.member.nickname){
-              uEmbed.addField("Nickname", message.author.username, true)
-            } else {
-              uEmbed.addField("Nickname", message.member.nickname, true)
-            }
-          uEmbed.setThumbnail(message.author.displayAvatarURL)
-          uEmbed.addField(`Currently playing`, message.author.presence.game ? message.author.presence.game.name: "Nothing", true)
-          uEmbed.addField("Roles", message.member.roles.map(r => `${r}`).join(' | '))
-          uEmbed.addField("Account created on", userCreated, true)
-          uEmbed.addField("Joined server on", userJoined, true)
-          message.channel.send(uEmbed);
-
-
-     } else { //if someone was mentioned
-
-        var memberToUse = message.mentions.members.first();
-        let userStats = memberToUse.presence.status;
-        var joinDiscord = moment(memberToUse.user.createdAt).format('llll');
-        var joinServer = moment(memberToUse.joinedAt).format('llll');
-
-        let userEmbed = new discord.RichEmbed()
-
-        userEmbed.setTitle(`Information for ${memberToUse.user.username}`)
-        switch(userStats){
-            case "online":
-              userEmbed.setDescription(`${online}**Online**`);
-              break;
-            case "dnd":
-              userEmbed.setDescription(`${dnd}**Do Not Disturb**`);
-              break;
-            case "idle":
-              userEmbed.setDescription(`${idle}**Idle**`);
-              break;
-            case "streaming":
-              userEmbed.setDescription(`${streaming}**Streaming**`);
-              break;
-            default:
-              userEmbed.setDescription(`${offline}**Invisible or offline**`);
-              break;
-          }
-      
-        userEmbed.addField("Username", `${memberToUse.user.tag}`, true)
-        userEmbed.setColor(color)
-        userEmbed.addField("UserID", `${memberToUse.id}`, true)
-        if(!memberToUse.nickname){
-            userEmbed.addField("Nickname", memberToUse.user.username, true)
+        uEmbed.addField("Username", `${message.author.tag}`, true)
+        uEmbed.setColor(color)
+        uEmbed.addField("UserID", `${message.author.id}`, true)
+        if(!message.member.nickname){
+            uEmbed.addField("Nickname", message.author.username, true)
           } else {
-            userEmbed.addField("Nickname", memberToUse.nickname, true)
+            uEmbed.addField("Nickname", message.member.nickname, true)
           }
-        userEmbed.setThumbnail(memberToUse.user.displayAvatarURL)
-        userEmbed.addField(`Currently playing`, memberToUse.presence.game ? memberToUse.presence.game.name: "Nothing", true)
-        userEmbed.addField("Roles", memberToUse.roles.map(r => `${r}`).join(' | '))
-        userEmbed.addField("Account created on", joinDiscord, true)
-        userEmbed.addField("Joined server on", joinServer, true)
-        message.channel.send(userEmbed);
-     }
+        uEmbed.setThumbnail(message.author.displayAvatarURL)
+        uEmbed.addField(`Currently playing`, message.author.presence.game ? message.author.presence.game.name: "Nothing", true)
+        uEmbed.addField("Roles", message.member.roles.map(r => `${r}`).join(' | '))
+        uEmbed.addField("Account created on", userCreated, true)
+        uEmbed.addField("Joined server on", userJoined, true)
+        message.channel.send(uEmbed);
+
+
+    } else { //if someone was mentioned
+
+      let userStats = mention.presence.status;
+      var joinDiscord = moment(mention.user.createdAt).format('llll');
+      var joinServer = moment(mention.joinedAt).format('llll');
+
+      let userEmbed = new discord.RichEmbed()
+
+      userEmbed.setTitle(`Information for ${mention.user.username}`)
+      switch(userStats){
+          case "online":
+            userEmbed.setDescription(`${online}**Online**`);
+            break;
+          case "dnd":
+            userEmbed.setDescription(`${dnd}**Do Not Disturb**`);
+            break;
+          case "idle":
+            userEmbed.setDescription(`${idle}**Idle**`);
+            break;
+          case "streaming":
+            userEmbed.setDescription(`${streaming}**Streaming**`);
+            break;
+          default:
+            userEmbed.setDescription(`${offline}**Invisible or offline**`);
+            break;
+        }
+      
+      userEmbed.addField("Username", `${mention.user.tag}`, true)
+      userEmbed.setColor(color)
+      userEmbed.addField("UserID", `${mention.id}`, true)
+      if(!mention.nickname){
+          userEmbed.addField("Nickname", mention.user.username, true)
+        } else {
+          userEmbed.addField("Nickname", mention.nickname, true)
+        }
+      userEmbed.setThumbnail(mention.user.displayAvatarURL)
+      userEmbed.addField(`Currently playing`, mention.presence.game ? mention.presence.game.name: "Nothing", true)
+      userEmbed.addField("Roles", mention.roles.map(r => `${r}`).join(' | '))
+      userEmbed.addField("Account created on", joinDiscord, true)
+      userEmbed.addField("Joined server on", joinServer, true)
+      message.channel.send(userEmbed);
+    }
 }
 
 module.exports.help = {
--- Say/get.js
+++ Say/get.js
@@ -1,29 +1,26 @@
 const discord = require('discord.js');
-const fs = require("fs");
 
 module.exports.run = async (bot, message, args) => {
     try{
 
-    if(message.author.id == '247979662314045441') return message.channel.send("no more get or record 4 u");
-    let user = message.mentions.members.first(); //user var is whoever is the first pinged person in the message
-    var mention = message.mentions.members.first() || message.guild.members.get(args[0]);
+        if(message.author.id == '247979662314045441') return message.channel.send("no more get or record 4 u");
+        var mention = message.mentions.members.first() || message.guild.members.get(args[0]);
 
-    if(mention == null){
-        try{ //tries to fetch the last recorded msg by the user
-        let aMessage = bot.whosmard[message.author.id].message;
-        return message.channel.send("last message recorded by " + message.author + " was --" + aMessage);
-        } catch(e){ //if the user nvr recorded a msg
-            return message.channel.send("u nvr recorded a msg"); //yey no errors
+        if(mention == null){
+            try{ //tries to fetch the last recorded msg by the user
+                let aMessage = bot.whosmard[message.author.id].message;
+                return message.channel.send("last message recorded by **" + message.author.tag + "** was --" + aMessage);
+            } catch(e){ //if the user nvr recorded a msg
+                return message.channel.send("u nvr recorded a msg"); //yey no errors
         }
     }
 
-    let _message = bot.whosmard[mention.id].message; //looks for the last message the person that got pinged sent
-    let userN = user.toString(); //puts the author ID into their username
+        let _message = bot.whosmard[mention.id].message; //looks for the last message the person that got pinged sent
 
-    message.channel.send("last message recorded by " + userN + " was --" + _message); //sends that message
-    } catch(e){
-        return message.channel.send("``" + mention.user.tag + "``" + " has not recorded a message")
-    }
+        message.channel.send("last message recorded by **" + mention.user.tag + "** was --" + _message); //sends that message
+        } catch(e){
+            return message.channel.send("``" + mention.user.tag + "``" + " has not recorded a message")
+        }
 
     };
 
